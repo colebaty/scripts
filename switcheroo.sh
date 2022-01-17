a070ebb6d95f6c34c604febb5f1b6f6a6ab104d7
Cole <tbaty002@odu.edu>
2022-01-17 17:46:06 -0500

    first commit

---
+++ switcheroo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/expect -f
+
+# script to help automate common tasks at ODU's ITS Networking Department
+#
+# usage:    ./switcheroo <user> <switch> <action>
+#   <user>              the `gpadmin` username
