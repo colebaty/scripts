807d2405b93dee94426917357e8be66c87f21c2e
Cole <cole.baty@gmail.com>
2022-01-20 17:08:03 -0500

    get user info

--- its/findUsername.sh
+++ its/findUsername.sh
@@ -2,7 +2,7 @@
 
 dbs=(ahosts ahostsv4 ahostsv6 aliases ethers group gshadow hosts initgroups netgroup networks passwd protocols rpc services shadow)
 
-for n in $dbs; do
+for n in $(!$dbs[@]); do
     #getent "$dbs[n]"
     echo "$dbs[$n]"
 done
